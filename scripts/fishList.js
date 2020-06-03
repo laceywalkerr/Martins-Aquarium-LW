46730e09a60d0337270ff2dcda0da3261e42cfa9
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-03 13:10:37 -0500

    adressing the request martin made about having his fish divisable by 3, 5, and not at all.

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -15,5 +15,54 @@ const fishList = () => {
     
         //Put the fish HTML representation inside the <carrier>
         fishArticleElement.innerHTML += fishHTML
+
+
+        
     }
+}
+
+
+
+
+
+
+
+
+// Function to show holy fish in the browser
+const showHolyFish = () => {
+    const fishObjectsArray = mostHolyFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fish(fishObject)
+        contentTarget.innerHTML += fishHTMLRepresentation
+    }
+}
+
+// Function to show soldier fish in the browser
+const showSoldierFish  = () => {
+    const fishObjectsArray = bestSoldierFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fish(fishObject)
+        contentTarget.innerHTML + fishHTMLRepresentation
+    }
+}
+
+// Function to show common fish in the browser
+
+const showRegularFish  = () => {
+    const fishObjectsArray = nonHolyFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fish(fishObject)
+        contentTarget.innerHTML + fishHTMLRepresentation
+    }
+}
+
+const fishList = () => {
+    // Invoke all three functions here
+    showHolyFish()
+    showSoldierFish()
+    showRegularFish()
+    
 }
